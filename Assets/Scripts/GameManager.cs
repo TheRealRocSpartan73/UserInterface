539d9ee1d7580c614ac6bf86729fabb42b536fde
TheRealRocSpartan73 <146456843+TheRealRocSpartan73@users.noreply.github.com>
2023-11-05 20:24:21 +0000

    5.1.6 Fin

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,16 +6,33 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
+
+    private float spawnRate = 1.0f;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Start the function to randomly spawn assets
+        StartCoroutine(SpawnTarget());
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(spawnRate); //Wait then spawn
+            int index = Random.Range(0, targets.Count); //Select Random object from list
+            Instantiate(targets[index]); //Finally create the object  -object will bounce based on the logic within Target.cs
+        }
+
+       
+    
+    
     }
 }
