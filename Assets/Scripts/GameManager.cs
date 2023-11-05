dbc47491a7addc3f8af2a4176b1497959de7044b
TheRealRocSpartan73 <146456843+TheRealRocSpartan73@users.noreply.github.com>
2023-11-05 21:02:10 +0000

    5.2.4 Fin

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -11,21 +11,22 @@ public class GameManager : MonoBehaviour
 
     private float spawnRate = 1.0f;
     private int score = 0;
-    
+
     // Start is called before the first frame update
     void Start()
     {
         //Start the function to randomly spawn assets
         StartCoroutine(SpawnTarget());
-        scoreText.text = "Score: " + score;
+        UpdateScore(0); //Just to be sure -- even though variable is instantiated at 0;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
+    //Spawns random objects
     IEnumerator SpawnTarget()
     {
         while (true)
@@ -33,10 +34,15 @@ public class GameManager : MonoBehaviour
             yield return new WaitForSeconds(spawnRate); //Wait then spawn
             int index = Random.Range(0, targets.Count); //Select Random object from list
             Instantiate(targets[index]); //Finally create the object  -object will bounce based on the logic within Target.cs
+            UpdateScore(5);//test
         }
 
-       
-    
-    
     }
+
+    void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
 }
