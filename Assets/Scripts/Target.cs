9102d5c9f76796c4b702eacbab1d3f15c40759bb
TheRealRocSpartan73 <146456843+TheRealRocSpartan73@users.noreply.github.com>
2023-11-05 20:03:57 +0000

    5.1.4 Complete

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -6,19 +6,26 @@ public class Target : MonoBehaviour
 {
 
     private Rigidbody targetRb;
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTorque = 10;
+    private float xRange = 4;
+    private float ySpawnPos = -6;
+    private float zSpawnPos = 0;
+
     // Start is called before the first frame update
     void Start()
     {
         targetRb= GetComponent<Rigidbody>();  //grab this objects RigidBody component.
         
         //Move object up based on random range. Use Impulse for instant Force Power
-        targetRb.AddForce(Vector3.up * Random.Range(12, 16), ForceMode.Impulse);
+        targetRb.AddForce(RandomForce(), ForceMode.Impulse);
 
-        //Rotate and Move based on random ranges on X, Y & Z axis.
-        targetRb.AddTorque(Random.Range(-10, 10), Random.Range(-10, 10), Random.Range(-10, 10), ForceMode.Impulse);
+        //Rotate and Move based on random ranges on X, Y & Z axis. Use Impulse for instant Force Power
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
 
         //RandomSpawn Point
-        this.transform.position = new Vector3(Random.Range(-4, 4), -6, 0);
+        this.transform.position = RandomSpawnPos();
 
     }
 
@@ -27,4 +34,22 @@ public class Target : MonoBehaviour
     {
         
     }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+
+    Vector3 RandomSpawnPos()
+    {
+      return new Vector3(Random.Range(-xRange, xRange), ySpawnPos, zSpawnPos);
+    }
+
+
+    float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+
+
 }
